20b680337ab3264cd0608a497bee70b7e0d8c10f
Pradeep Rajput <pradeep87055@gmail.com>
2023-01-05 23:03:44 +0530

    setup complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React,{useEffect} from 'react';
+import { BrowserRouter,Route,Routes }from 'react-router-dom'
+import {HomePage}from './pages'
+import {Navbar} from './components'
+import WebFont, { load } from 'webfontloader';
+
 
 const App = () => {
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Inter"],
+      },
+    });
+  }, []);
+
   return (
-    <div>App</div>
+   <BrowserRouter>
+    <Navbar/>
+   <Routes>
+      <Route path='/' element={<HomePage/>} />
+   </Routes>
+   </BrowserRouter>
   )
 }
 
---
+++ src/constants/Constants.js
@@ -0,0 +1,13 @@
+
+
+
+export const pageRoutes = {
+    home: "/",
+    userHome: "/home",
+    communities: "/communities",
+    signIn: "/singIn",
+    signUp: "/signUp",
+    chat: "/chat",
+    classes: "/classes",
+    profile: "/profile",
+}
