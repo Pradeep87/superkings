5727b24ad7a14b92fe9382da05956ff1826b7ff0
Pradeep Rajput <pradeep87055@gmail.com>
2023-01-06 17:16:56 +0530

    slider super stores card added

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -5,32 +5,69 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper";
+import { catIcon } from "../../assets";
+import { AiOutlineInsertRowLeft, AiOutlineLineChart, AiOutlineFork, AiOutlineControl } from "react-icons/ai";
 
 
 
-const images=[
+
+const SuperStoresData = [
   {
-   img :'https://qcs24.com/assets/gallery/1669478466176_112b1bdb-0e77-4137-aad8-6a54888f360a__UR3000,600_SX3000_FMwebp_.jpg'
+    icon: <AiOutlineInsertRowLeft className="reactIcons" />,
+    storeName: "Bajaj Mall"
   },
- 
   {
-   img :"https://qcs24.com/assets/gallery/1669478424415_31facd80-5bdc-4de8-9035-74836ecdb7f5__UR3000,600_SX3000_FMwebp__(1)_(1).jpg"
+    icon: <AiOutlineLineChart className="reactIcons" />,
+    storeName: "Investments"
   },
   {
-    img :'https://qcs24.com/assets/gallery/1669478466176_112b1bdb-0e77-4137-aad8-6a54888f360a__UR3000,600_SX3000_FMwebp_.jpg'
-   },
-  
-  {
-   img :"https://qcs24.com/assets/gallery/1669478424415_31facd80-5bdc-4de8-9035-74836ecdb7f5__UR3000,600_SX3000_FMwebp__(1)_(1).jpg"
+    icon: <AiOutlineFork className="reactIcons" />,
+    storeName: "Insurance"
   },
-  
-
-  
+  {
+    icon: <AiOutlineControl className="reactIcons" />,
+    storeName: "Securities"
+  },
 ]
 
-const HomePageSection=()=>{
-  return(
+
+const images = [
+  {
+    img: 'https://qcs24.com/assets/gallery/1669478466176_112b1bdb-0e77-4137-aad8-6a54888f360a__UR3000,600_SX3000_FMwebp_.jpg'
+  },
+
+  {
+    img: "https://qcs24.com/assets/gallery/1669478424415_31facd80-5bdc-4de8-9035-74836ecdb7f5__UR3000,600_SX3000_FMwebp__(1)_(1).jpg"
+  },
+  {
+    img: 'https://qcs24.com/assets/gallery/1669478466176_112b1bdb-0e77-4137-aad8-6a54888f360a__UR3000,600_SX3000_FMwebp_.jpg'
+  },
+
+  {
+    img: "https://qcs24.com/assets/gallery/1669478424415_31facd80-5bdc-4de8-9035-74836ecdb7f5__UR3000,600_SX3000_FMwebp__(1)_(1).jpg"
+  },
+]
+
+const SuperStoreCard = () => {
+  return (
+    <div className="SuperStoreCard">
+      <h4>Superstores</h4>
+      <ul>
+        {
+          SuperStoresData.map((d, idx) => <li key={idx}>{d.icon}{d.storeName}</li>)
+        }
+      </ul>
+
+    </div>
+  )
+}
+
+
+const HomePageSection = () => {
+  return (
     <div className='homePageSections' >
+      <SuperStoreCard />
+
       <Swiper
         slidesPerView={1}
         spaceBetween={30}
@@ -42,17 +79,14 @@ const HomePageSection=()=>{
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-
         {
-images.map((data)=><SwiperSlide>
-<img src={data.img} alt="" />
-        </SwiperSlide>
-)
-
+          images.map((data, idx) => <SwiperSlide key={idx} >
+            <img src={data.img} alt="" className="sliderImage" />
+          </SwiperSlide>
+          )
         }
-       
-      
       </Swiper>
+
 
     </div>
   )
@@ -62,9 +96,7 @@ images.map((data)=><SwiperSlide>
 const HomePage = () => {
   return (
     <div className='homePage' >
-
-<HomePageSection/>
-
+      <HomePageSection />
     </div>
   )
 }
